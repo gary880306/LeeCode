0434a7f973f58e850d91f7885dc004acbc447974
Gary <ch66971542@gmail.com>
2024-02-26 16:37:19 +0800

    Easy PalindromeNumber.java

---
+++ Easy/PalindromeNumber.java
@@ -0,0 +1,24 @@
+package Easy;
+
+public class PalindromeNumber {
+    public static boolean palindromeNumber(int x){
+        if(x < 0 || (x % 10 == 0 && x != 0)){
+            return false;
+        }
+
+        // r = reverse
+        int r = 0;
+        while(x > r){
+            r = r * 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == r || x == (r / 10);
+    }
+
+    public static void main(String[] args) {
+        int x = 121;
+        boolean result = palindromeNumber(x);
+        System.out.println(result);
+    }
+}
